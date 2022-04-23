cb30f8e4a59b5bb9bb333866f6a34bad6b2ff151
Krasnonos <andreykrasnonos@icloud.com>
2022-04-23 14:55:29 +0300

    fix Markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,16 +7,54 @@ const galleryEl = document.querySelector('.gallery')
 
 
 function createImagesMarkup(imgArray) {
-    return imgArray.map(({preview, original, description}) => {
-        const image = document.createElement('img');
+    return imgArray.map(({ preview, original, description }) => {
+        const linkEl = document.createElement('a')
+        linkEl.classList.add('gallery__link');
+        linkEl.href = original;
 
-        image.src = preview;
-        image.dataset.fullSrc = original;
-        image.classList.add('gallery__image')
-        image.alt = description;
+        const imageEl = document.createElement('img');
+        imageEl.classList.add('gallery__image')
+        imageEl.src = preview;
+        imageEl.dataset.source = original;
+        imageEl.alt = description;
 
-        return image;
+        linkEl.append(imageEl);
+
+        return linkEl;
     })
 }
 
 galleryEl.append(...createImagesMarkup(galleryItems))
+
+
+
+
+
+
+
+
+
+
+
+
+// function createImagesMarkup(imgArray) {
+//     return imgArray.map(({ preview, original, description }) => {
+//         const linkEl = document.createElement('a');
+//         linkEl.classList.add('gallery__link');
+//         linkEl.href = original;
+        
+//         const imageEl = document.createElement('img');
+//         imageEl.classList.add('gallery__image')
+//         imageEl.src = preview;
+//         imageEl.dataset.source = original;
+//         imageEl.alt = description;
+
+
+//         linkEl.append(imageEl);
+        
+//         return linkEl;
+//    })
+    
+// }
+
+// galleryEl.append(...createImagesMarkup(galleryItems))
