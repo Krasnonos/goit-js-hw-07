3b63690acf77a9493f1e1fc3e6bc61594040ac5c
Krasnonos <andreykrasnonos@icloud.com>
2022-04-23 18:12:32 +0300

    set lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,23 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-
 const galleryEl = document.querySelector('.gallery')
 
 
 function createImagesMarkup(imgArray) {
     return imgArray.map(({ preview, original, description }) => {
-        const linkEl = document.createElement('a')
-        linkEl.classList.add('gallery__link');
-        linkEl.href = original;
+        const linkEl = createLink(original);
 
-        const imageEl = document.createElement('img');
-        imageEl.classList.add('gallery__image')
-        imageEl.src = preview;
-        imageEl.dataset.source = original;
-        imageEl.alt = description;
-
+        const imageEl = createImg(preview, original, description)
+        
         linkEl.append(imageEl);
 
         return linkEl;
@@ -25,6 +17,49 @@ function createImagesMarkup(imgArray) {
 }
 
 galleryEl.append(...createImagesMarkup(galleryItems))
+
+galleryEl.addEventListener('click', onOpenModal)
+
+// -------------SIDE-FUNCTIONS --------------
+
+
+function onOpenModal(evt) {
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+    
+    const parrentEl = evt.target.parentNode.href;
+
+    const modalImg = basicLightbox.create(`
+        <img src="${parrentEl}" width="800" height="600">
+    `)
+
+    modalImg.show(modalImg)
+
+}
+
+function createLink (href) {
+    const linkEl = document.createElement('a')
+        linkEl.classList.add('gallery__link');
+        linkEl.href = href;
+    return linkEl;
+};
+
+function createImg (src, data, alt) {
+    const imageEl = document.createElement('img');
+        imageEl.classList.add('gallery__image')
+        imageEl.src = src;
+        imageEl.dataset.source = data;
+        imageEl.alt = alt;
+    return imageEl;
+}
+
+
+
+
+
 
 
 
