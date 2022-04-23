517ac68ba5b4f1f6128d6f3f47dec523ed4f2ff1
Krasnonos <andreykrasnonos@icloud.com>
2022-04-23 14:31:28 +0300

    createImgMarkup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+
+
+function createImagesMarkup(imgArray) {
+    return imgArray.map(({preview, original, description}) => {
+        const image = document.createElement('img');
+
+        image.src = preview;
+        image.dataset.fullSrc = original;
+        image.alt = description;
+
+        return image;
+    })
+}
+
+galleryEl.append(...createImagesMarkup(galleryItems))
