3ba5756f08d8889dfdb51f895549e9a6fe439897
Krasnonos <andreykrasnonos@icloud.com>
2022-04-23 19:30:04 +0300

    start task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryLestEl = document.querySelector('.gallery')
+
+function createImagesMarkup (imgArray) {
+    return imgArray.map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`
+    }).join('')
+}
+
+galleryLestEl.innerHTML = createImagesMarkup(galleryItems);
+
+// -----------------MODAL-FUNCTION-----------------
+
+// galleryLestEl.addEventListener('click', onOpenModal);
+
+// function onOpenModal(evt) {
+//     evt.preventDefault()
+
+//     const lightbox = new SimpleLightbox('.gallery a', { /* options */ });
+// }
